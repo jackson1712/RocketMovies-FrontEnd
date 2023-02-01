d743586560c6ff8bb71f1f6841695ab401953de1
Jackson <jackson1712@hotmail.com>
2023-02-01 15:40:00 -0300

    component added in pages to main title

---
+++ src/components/TitlePage/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.h1`
+    font-weight: 700;
+    font-size: 2.4rem;
+    color: ${({ theme }) => theme.COLORS.PINK};
+    border: none;
+    background: none;
+`;
