544078a809d0065627e45a5c284b00bd80703966
Jackson <jackson1712@hotmail.com>
2023-02-04 19:44:25 -0300

    refactored to receive data of database

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { api } from "../../services/api";
 
 import { Container, Form, Background } from "./styles";
 import { Input } from "../../components/Input";
@@ -8,7 +9,28 @@ import { Button } from "../../components/Button";
 import { useNavigate } from "react-router-dom";
  
 export function SignUp() {
-    const [] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSignUp(){
+        if(!name || !email || !password) {
+            return alert("Preencha todos os campos!");
+        }
+
+        api.post("/users", {name, email, password})
+        .then(() => {
+            alert("Conta criada com sucesso!")
+            navigate(-1)
+        })
+        .catch(error => {
+            if(error.response){
+                alert(error.response.data.message);
+            }else{
+                alert("Não foi possível cadastrar usuário")
+            }
+        });
+    }
 
     const navigate = useNavigate();
 
@@ -29,23 +51,29 @@ export function SignUp() {
                 icon={FiUser} 
                 type="text" 
                 placeholder="Nome"
+                onChange={e => setName(e.target.value)}
                 />
 
                 <Input 
                 icon={FiMail} 
                 type="text" 
                 placeholder="E-mail"
+                onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input 
                 icon={FiLock}
                 type="password" 
                 placeholder="Senha"
+                onChange={e => setPassword(e.target.value)}
                 />
 
                 </main>
 
-                <Button title="Cadastrar" />
+                <Button 
+                title="Cadastrar" 
+                onClick={handleSignUp}
+                />
 
                 <footer>
                 <ButtonText
